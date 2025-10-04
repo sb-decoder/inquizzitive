e6266ee562907fb618225b4d87b9adfcd04a6394
code-soubhik <soubhikbanerjee04@gmail.com>
2025-10-05 00:44:07 +0530

    Security Fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_BASE_URL, // Express backend
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '')
+      },
+    },
+  },
   plugins: [react()],
   build: {
     // Production optimizations
